7cef47d5aebeda646b669502cced8c2413927553
sabina489 <Karkisabina869@gmail.com>
2022-01-27 11:48:00 +0545

    learn automated testing

--- polls/views.py
+++ polls/views.py
@@ -30,6 +30,11 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
