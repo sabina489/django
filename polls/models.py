71a2527f40613c8cc5a3f8de2101d260312a33ce
sabina489 <Karkisabina869@gmail.com>
2022-01-30 18:44:35 +0545

    customize admin site

--- polls/models.py
+++ polls/models.py
@@ -2,12 +2,22 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        
 
 # Create your models here.
 class Question(models.Model):
@@ -15,6 +25,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+        
 
 
 class Choice(models.Model):
